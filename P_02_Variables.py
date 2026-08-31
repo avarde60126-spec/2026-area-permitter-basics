70a1701f0bf624f88a4a487e9e817cbb59b82b88
avarde60126-spec <avarde60126@masseyhigh.school.nz>
2026-09-01 11:49:23 +1200

    Add files via upload

---
+++ P_02_Variables.py
@@ -0,0 +1,16 @@
+# Code to Learn about Variables
+
+# Variables can be a string (note the speech marks)
+Greeting = "hello world"
+
+# Variables can be a number
+fav_num = 7
+num_string = "5"
+
+print(fav_num * 2)
+print(num_string * 5)
+
+# Costants are uppercase
+PI = 3.14
+
+a_01_my_num = 7
